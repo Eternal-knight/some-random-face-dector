519266f81eef08e6181cd55f2ede81c782e8e1b0
Fardin Samir D <etrnlnite@pm.me>
2022-07-12 23:55:05 +0530

    new new new

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -54,53 +54,6 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 			locs.append((startX, startY, endX, endY))
 
 	
-	if len(faces) > 0:
-		
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32)
-
-	
-maskNet = load_model("mask_detector.model")
-
-#video
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-
-
-while True:
-
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
-
-
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-
-	
-	for (box, pred) in zip(locs, preds):
-		
-		(startX, startY, endX, endY) = box
-		(mask, withoutMask) = pred
-
-		
-		label = "Mask" if mask > withoutMask else "No Mask"
-		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-		
-		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-		
-		# frame
-		cv2.putText(frame, label, (startX, startY - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-
-	
-	if key == ord("q"):
-		break
-
 
 cv2.destroyAllWindows()
 vs.stop()
