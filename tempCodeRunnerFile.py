2790ab303ddff7b4f5f00325c8265d215d106aef
Fardin Samir D <etrnlnite@pm.me>
2022-08-05 23:22:22 +0530

    aug 5 commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -73,8 +73,7 @@ aug = ImageDataGenerator(
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
+
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
