45741bd3f845f841f4768536f6390634205fcb52
Fardin Samir D <etrnlnite@pm.me>
2022-08-19 11:00:41 +0530

    aug 19 commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -77,7 +77,7 @@ baseModel = MobileNetV2(weights="imagenet", include_top=False,
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
-headModel = Dense(128, activation="relu")(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
